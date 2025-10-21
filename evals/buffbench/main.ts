7339ae8dd8105c90aba3d2a433cf0cff4d95e88d
James Grugett <jahooma@gmail.com>
2025-10-21 16:07:19 -0700

    Tweak base2-gpt-5 worker

--- evals/buffbench/main.ts
+++ evals/buffbench/main.ts
@@ -5,7 +5,7 @@ import { runBuffBench } from './run-buffbench'
 async function main() {
   await runBuffBench({
     evalDataPath: path.join(__dirname, 'eval-codebuff.json'),
-    agents: ['base2-fast', 'base2', 'base-max'],
+    agents: ['base2-fast', 'base2', 'base2-max'],
     taskConcurrency: 10,
   })
 

57f6230b220701afd17960f116a6da917d35accb
James Grugett <jahooma@gmail.com>
2026-03-08 22:45:50 -0700

    Fix types

--- freebuff/web/src/components/theme-provider.tsx
+++ freebuff/web/src/components/theme-provider.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from 'next-themes'
-import { useEffect } from 'react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { type ComponentProps, useEffect } from 'react'
+
+type ThemeProviderProps = ComponentProps<typeof NextThemesProvider>
 
 export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
   useEffect(() => {
